8443bf8c0347349ab6ae4054d8ea2cc23418262a
Amir Aghdam <amir.aghdam@axxessio.com>
2023-06-07 19:33:05 +0200

    First working app

---
+++ backend/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, request, render_template
+import json
+from pathlib import Path
+
+# Get the directory where this file is located
+BASE_DIR = Path(__file__).resolve().parent
+
+FRONTEND_DIR = (BASE_DIR / '..' / 'frontend').resolve()
+STATIC_DIR = (FRONTEND_DIR / 'static').resolve()
+TEMPLATES_DIR = (FRONTEND_DIR / 'templates').resolve()
+
+DATABASE_DIR = (BASE_DIR / '..' / 'database').resolve()
+
+app = Flask(__name__,
+            static_folder=STATIC_DIR,
+            template_folder=TEMPLATES_DIR)
+
+# Load quizzes from JSON file
+with open((DATABASE_DIR / 'quizzes.json').resolve()) as f:
+    quizzes = json.load(f)
+
+@app.route('/')
+def home():
+    return render_template('quiz.html')
+
+
+@app.route('/quiz/<int:index>', methods=['GET'])
+def get_quiz(index):
+    # Return the quiz at the given index
+    if 0 <= index < len(quizzes):
+        return jsonify(quizzes[index])
+    else:
+        return "Invalid quiz index.", 404
+
+
+@app.route('/quiz/total', methods=['GET'])
+def get_total_quizzes():
+    # Return the total number of quizzes
+    return jsonify({'total': len(quizzes)})
+
+
+@app.route('/quiz', methods=['POST'])
+def check_answers():
+    # Assume the request data is a dictionary where keys are quiz indices
+    # and values are the user's answers
+    user_answers = request.json
+
+    # Check each answer and count how many are correct
+    correct_count = 0
+    for index, user_answer in user_answers.items():
+        if 0 <= index < len(quizzes):
+            correct_answer = quizzes[index]['answer']
+            if user_answer == correct_answer:
+                correct_count += 1
+
+    # Return the number of correct answers
+    return jsonify({'correct_count': correct_count})
+
+
+@app.route('/quiz/check_answers', methods=['POST'])
+def check_all_answers():
+    # Assume the request data is a dictionary where keys are quiz indices
+    # and values are the user's answers
+    user_answers = request.json
+
+    # Check each answer and count how many are correct
+    correct_count = 0
+    for index_str, user_answer in user_answers.items():
+        index = int(index_str)
+        if 0 <= index < len(quizzes):
+            correct_answer = quizzes[index]['answer']
+            if user_answer == correct_answer:
+                correct_count += 1
+
+    # Return the number of correct answers
+    return jsonify({'correct_count': correct_count})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/static/js/quiz.js
@@ -0,0 +1,96 @@
+// Get DOM elements
+const quizContainer = document.getElementById('quiz-container');
+const submitButton = document.getElementById('submit-button');
+const nextButton = document.getElementById('next-button');
+const resultDiv = document.getElementById('result');
+
+// Initialize quiz index, user answers, and total quizzes
+let quizIndex = 0;
+let userAnswers = {};
+let totalQuizzes = 0;
+
+// Fetch total quizzes from backend
+fetch('/quiz/total')
+    .then(response => response.json())
+    .then(data => {
+        totalQuizzes = data.total;
+
+        // Load first quiz from backend
+        loadQuiz(quizIndex);
+    });
+
+function loadQuiz(index) {
+    // Clear quiz container
+    quizContainer.innerHTML = '';
+
+    // Load quiz from backend
+    fetch('/quiz/' + index)
+        .then(response => response.json())
+        .then(quiz => {
+            // Display quiz on page
+            const questionDiv = document.createElement('div');
+            questionDiv.className = 'question';
+            questionDiv.textContent = quiz.question;
+
+            const optionsUl = document.createElement('ul');
+            optionsUl.className = 'options';
+
+            for (let option of quiz.options) {
+                const li = document.createElement('li');
+                const label = document.createElement('label');
+                const radio = document.createElement('input');
+                radio.type = 'radio';
+                radio.name = quiz.question;
+                radio.value = option;
+                label.appendChild(radio);
+                label.appendChild(document.createTextNode(option));
+                li.appendChild(label);
+                optionsUl.appendChild(li);
+            }
+
+            questionDiv.appendChild(optionsUl);
+            quizContainer.appendChild(questionDiv);
+
+            // Increment quiz index and update buttons
+            quizIndex++;
+            if (quizIndex >= totalQuizzes) {
+                nextButton.style.display = 'none';
+                submitButton.style.display = 'block';
+            }
+        });
+}
+
+// Handle next button click
+nextButton.addEventListener('click', () => {
+    // Collect user's answer
+    const userAnswer = document.querySelector('input[type=radio]:checked').value;
+    userAnswers[quizIndex - 1] = userAnswer;  // Use quizIndex - 1 because quizIndex was already incremented in loadQuiz
+
+    // Load next quiz
+    if (quizIndex < totalQuizzes) {
+        loadQuiz(quizIndex);
+    }
+});
+
+// Handle submit button click
+submitButton.addEventListener('click', () => {
+    // Collect user's answer for the last question
+    const userAnswer = document.querySelector('input[type=radio]:checked').value;
+    userAnswers[quizIndex - 1] = userAnswer;  // Use quizIndex - 1 because quizIndex was already incremented in loadQuiz
+
+    // Send user's answers to backend
+    fetch('/quiz/check_answers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userAnswers)
+    })
+        .then(response => response.json())
+        .then(result => {
+            // Display result
+            resultDiv.textContent = 'You got ' + result.correct_count + ' correct answers.';
+        });
+});
+
+
